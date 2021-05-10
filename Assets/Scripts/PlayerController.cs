da976b6898bdbc5452679868a1a9cc64a3baec5f
Gabriel Zezze <gabriel@zezze.dev>
2021-05-10 15:32:10 -0300

    pickup sfx

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,62 +6,64 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-   float _baseSpeed = 10.0f;
+    float _baseSpeed = 10.0f;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
-   GameObject playerCamera;
-   GameManager gm;
-   float cameraRotation;
-   private Vector3 moveDirection = Vector3.zero;
-   private int jumps = 0;
-   private float vibration_time = 0.0f;
+    GameObject playerCamera;
+    GameManager gm;
+    float cameraRotation;
+    private Vector3 moveDirection = Vector3.zero;
+    private int jumps = 0;
+    private float vibration_time = 0.0f;
 
-   private float vibration = 0;
-   public Text hint;
+    private float vibration = 0;
+    public Text hint;
 
-   public GameObject bombDefuseHint;
+    public GameObject bombDefuseHint;
 
-   CharacterController characterController;
+    private AudioSource _audiosource;
+    CharacterController characterController;
 
-   void Start()
-   {
+    void Start()
+    {
         if (gm == null) {
             gm = GameManager.GetInstance();
         }
-       characterController = GetComponent<CharacterController>();
-       playerCamera = GameObject.Find("Main Camera");
-       cameraRotation = 0.0f;
-       Cursor.lockState = CursorLockMode.Locked;
-   }
+        characterController = GetComponent<CharacterController>();
+        playerCamera = GameObject.Find("Main Camera");
+        cameraRotation = 0.0f;
+        Cursor.lockState = CursorLockMode.Locked;
+        _audiosource = gameObject.GetComponent<AudioSource>();
+    }
 
-   void Update()
-   {
-       if (gm.game_paused){
-           Cursor.lockState = CursorLockMode.None;
-           return;
-       }
+    void Update()
+    {
+        if (gm.game_paused){
+            Cursor.lockState = CursorLockMode.None;
+            return;
+        }
 
-       Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
 
-       float x = Input.GetAxis("Horizontal");
-       float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-       float mouse_dX = Input.GetAxis("Mouse X");
-       float mouse_dY = Input.GetAxis("Mouse Y")*-1;
+        float mouse_dX = Input.GetAxis("Mouse X");
+        float mouse_dY = Input.GetAxis("Mouse Y")*-1;
 
-       vibration_time += Time.deltaTime;
+        vibration_time += Time.deltaTime;
 
-       if(Input.GetKey("left shift") && characterController.isGrounded){
-           _baseSpeed = 15.0f;
-           if(vibration_time > 0.005f){
+        if(Input.GetKey("left shift") && characterController.isGrounded){
+            _baseSpeed = 15.0f;
+            if(vibration_time > 0.005f){
             mouse_dY += Mathf.Sin(vibration)/20;
             vibration += 0.1f;  
             vibration_time = 0.0f;
-           }
-       } else{
-           vibration = 0.0f;
-           _baseSpeed = 10.0f;
-       }
+            }
+        } else{
+            vibration = 0.0f;
+            _baseSpeed = 10.0f;
+        }
 
         if (characterController.isGrounded) {
             jumps = 0;
@@ -82,14 +84,14 @@ public class PlayerController : MonoBehaviour
         characterController.Move(moveDirection * Time.deltaTime);
 
 
-       cameraRotation += mouse_dY;
-       Mathf.Clamp(cameraRotation, -75.0f, 75.0f);
-       
-       transform.Rotate(Vector3.up, mouse_dX);
+        cameraRotation += mouse_dY;
+        Mathf.Clamp(cameraRotation, -75.0f, 75.0f);
+        
+        transform.Rotate(Vector3.up, mouse_dX);
 
 
-       playerCamera.transform.localRotation = Quaternion.Euler(cameraRotation, 0.0f, 0.0f);
-   }
+        playerCamera.transform.localRotation = Quaternion.Euler(cameraRotation, 0.0f, 0.0f);
+    }
 
     void LateUpdate()
 
@@ -104,10 +106,12 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.F)){
             switch(hit.collider.name){
                 case "Knife":
+                    _audiosource.Play();
                     Destroy(hit.collider.gameObject);
                     gm.got_knife();
                     break;
                 case "Pergaminho":
+                    _audiosource.Play();
                     gm.set_bomb_hint(true);
                     bombDefuseHint.SetActive(true);
                     break;
