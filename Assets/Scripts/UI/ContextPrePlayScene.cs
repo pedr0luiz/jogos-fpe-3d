49ef0a0f081252165b356f685533a54cd5d4bc3e
Gabriel Zezze <gabriel@zezze.dev>
2021-05-10 15:16:40 -0300

    wip

---
+++ Assets/Scripts/UI/ContextPrePlayScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContextPrePlayScene : MonoBehaviour {
+    public void play_game() {
+        Debug.Log("AQUI");
+        SceneManager.LoadScene("PlayScene");
+    }
+
+}
